229c0bd5493a5a7263d468687edf7c3d1a71076e
ousttrue <ousttrue@gmail.com>
2022-01-31 18:36:57 +0900

    write_to

--- macro_cutter.py
+++ macro_cutter.py
@@ -1,4 +1,5 @@
 from typing import Optional, List, Iterator, Union, Tuple, NamedTuple
+import io
 import logging
 import pathlib
 import sys
@@ -7,29 +8,60 @@ logger = logging.getLogger(__name__)
 MACRO = re.compile(r'#\s*(\w+)(.*)')
 
 
+class Definition(NamedTuple):
+    name: str
+    value: str
+
+
 class Macro:
     def __init__(self, start: int, macro: str, arg: str) -> None:
         self.start = start+1
         self.macro = macro
         self.arg = arg
-        self.lines: List[Union[str, 'Scope']] = []
+        self.lines: List[Union[str, 'Scope', Definition]] = []
         #
-        self.end = None
+        self.end = -1
 
     def __str__(self) -> str:
         return f'[#{self.macro} {self.arg}, {self.start}: {self.end}]'
 
-    def push(self, end: int, l: Union[str, 'Scope']):
-        self.end = end + 1
-        self.lines.append(l)
+    def is_enable(self, definitions: List[str], any_prev: bool) -> bool:
+        match self.macro:
+            case 'if' | 'elif':
+                if self.arg.startswith('defined '):
+                    target = self.arg[8:].strip()
+                    if target in definitions:
+                        return True
+                return False
+            case 'ifdef':
+                target = self.arg.strip()
+                if target in definitions:
+                    return True
+                return False
+            case 'ifndef':
+                target = self.arg.strip()
+                if target not in definitions:
+                    return True
+                return False
+            case 'else':
+                if any_prev:
+                    return False
+                return True
+            case '':
+                return True
+            case _:
+                raise NotImplementedError()
 
 
 class Scope:
     def __init__(self, macro: Macro) -> None:
         self.macros: List[Macro] = [macro]
 
-    def __str__(self) -> str:
-        return f'{", ".join(str(m) for m in self.macros)}'
+    def push(self, end: int, l: Union[str, 'Scope', Definition, None] = None):
+        macro = self.macros[-1]
+        macro.end = end + 1
+        if l != None:
+            macro.lines.append(l)
 
     def parse(self, it: Iterator[Tuple[int, str]]):
         # copy
@@ -42,54 +74,74 @@ class Scope:
                         case 'ifdef':
                             child = Scope(Macro(i, m.group(1),
                                           m.group(2).strip()))
-                            self.macros[-1].push(i, child)
+                            self.push(i, child)
                             child.parse(it)
                         case 'ifndef':
                             child = Scope(Macro(i, m.group(1),
                                           m.group(2).strip()))
-                            self.macros[-1].push(i, child)
+                            self.push(i, child)
                             child.parse(it)
                         case 'if':
                             child = Scope(Macro(i, m.group(1),
                                           m.group(2).strip()))
-                            self.macros[-1].push(i, child)
+                            self.push(i, child)
                             child.parse(it)
                         case 'elif':
                             self.macros.append(
                                 Macro(i, m.group(1), m.group(2).strip()))
-                            self.macros[-1].push(i, l)
+                            self.push(i)
                         case 'else':
                             self.macros.append(
                                 Macro(i, m.group(1), ''))
-                            self.macros[-1].push(i, l)
+                            self.push(i)
                         case 'endif':
-                            self.macros[-1].push(i, l)
+                            self.push(i)
                             return
                         case 'define':
-                            self.macros[-1].push(i, l)
+                            # self.push(i, l)
+                            splitted = m.group(2).strip().split(maxsplit=1)
+                            self.push(i,
+                                      Definition(splitted[0], ''))
                         case 'include':
-                            self.macros[-1].push(i, l)
+                            self.push(i, l)
                         case _:
-                            self.macros[-1].push(i, l)
+                            self.push(i, l)
                 else:
-                    self.macros[-1].push(i, l)
+                    self.push(i, l)
 
             except StopIteration:
                 break
 
-    def print(self, definitions: List[str] = [], *, level=0):
+    def write_to(self, w: io.IOBase, definitions: List[str] = [], *, level=0):
         indent = '  ' * level
-        print(f'{indent}{self}')
+        any_prev = False
         for macro in self.macros:
+            if not macro.is_enable(definitions, any_prev):
+                # logging.debug(f'disable: {macro}: {definitions}')
+                continue
+            any_prev = True
             for child in macro.lines:
                 match child:
                     case Scope() as scope:
-                        scope.print(definitions, level=level+1)
+                        scope.write_to(w, definitions, level=level+1)
+
+                    case Definition() as d:
+                        logger.debug(d)
+                        definitions.append(d.name)
+
+                    case str() as l:
+                        w.write(l)
+                        w.write('\n')
+
+                    case _:
+                        raise RuntimeError()
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-    text = pathlib.Path(sys.argv[1]).read_text()
+    src = pathlib.Path(sys.argv[1]).read_text()
     root = Scope(Macro(0, '', ''))
-    root.parse(enumerate(text.splitlines()))
-    root.print(['NANOVG_GL2_IMPLEMENTATION'])
+    root.parse(enumerate(src.splitlines()))
+
+    dst = pathlib.Path(sys.argv[2])
+    root.write_to(dst.open('w'), ['NANOVG_GL3_IMPLEMENTATION'])
