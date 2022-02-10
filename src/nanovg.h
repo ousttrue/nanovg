a84be04559cc7ce3c3b1beb24e132cb3a8075eac
ousttrue <ousttrue@gmail.com>
2022-02-10 15:25:59 +0900

    nanovg_gl.h

--- src/nanovg.h
+++ src/nanovg.h
@@ -30,6 +30,22 @@ extern "C" {
 #pragma warning(disable: 4201)  // nonstandard extension used : nameless struct/union
 #endif
 
+// Create flags
+
+enum NVGcreateFlags
+{
+	// Flag indicating if geometry based anti-aliasing is used (may not be needed
+	// when using MSAA).
+	NVG_ANTIALIAS = 1 << 0,
+	// Flag indicating if strokes should be drawn using stencil buffer. The
+	// rendering will be a little
+	// slower, but path overlaps (i.e. self-intersecting or sharp turns) will be
+	// drawn just once.
+	NVG_STENCIL_STROKES = 1 << 1,
+	// Flag indicating that additional debug checks are done.
+	NVG_DEBUG = 1 << 2,
+};
+
 typedef struct NVGcontext NVGcontext;
 
 struct NVGcolor {
--- src/nanovg_gl_texture.cpp
+++ src/nanovg_gl_texture.cpp
@@ -1,19 +1,21 @@
 #include "nanovg_gl_texture.h"
 #include "nanovg.h"
 #include "nanovg_gl.h"
-#include <glad/glad.h>
 #include <assert.h>
+#include <glad/glad.h>
 
-GLNVGtexture::GLNVGtexture()
-{
+// These are additional flags on top of NVGimageFlags.
+enum NVGimageFlagsGL {
+  NVG_IMAGE_NODELETE = 1 << 16, // Do not delete GL texture handle.
+};
+
+GLNVGtexture::GLNVGtexture() {
   static int s_textureId = 0;
   _id = ++s_textureId;
 }
 
-GLNVGtexture::~GLNVGtexture()
-{
-  if (_handle != 0 && (_flags & NVG_IMAGE_NODELETE) == 0)
-  {
+GLNVGtexture::~GLNVGtexture() {
+  if (_handle != 0 && (_flags & NVG_IMAGE_NODELETE) == 0) {
     glDeleteTextures(1, &_handle);
   }
 }
@@ -22,8 +24,7 @@ void GLNVGtexture::bind() { glBindTexture(GL_TEXTURE_2D, _handle); }
 void GLNVGtexture::unbind() { glBindTexture(GL_TEXTURE_2D, 0); }
 
 std::shared_ptr<GLNVGtexture>
-GLNVGtexture::load(int w, int h, int type, const void *data, int imageFlags)
-{
+GLNVGtexture::load(int w, int h, int type, const void *data, int imageFlags) {
   auto tex = std::shared_ptr<GLNVGtexture>(new GLNVGtexture);
   glGenTextures(1, &tex->_handle);
   tex->bind();
@@ -44,37 +45,25 @@ GLNVGtexture::load(int w, int h, int type, const void *data, int imageFlags)
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, w, h, 0, GL_RED, GL_UNSIGNED_BYTE,
                  data);
 
-  if (imageFlags & NVG_IMAGE_GENERATE_MIPMAPS)
-  {
-    if (imageFlags & NVG_IMAGE_NEAREST)
-    {
+  if (imageFlags & NVG_IMAGE_GENERATE_MIPMAPS) {
+    if (imageFlags & NVG_IMAGE_NEAREST) {
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
                       GL_NEAREST_MIPMAP_NEAREST);
-    }
-    else
-    {
+    } else {
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
                       GL_LINEAR_MIPMAP_LINEAR);
     }
-  }
-  else
-  {
-    if (imageFlags & NVG_IMAGE_NEAREST)
-    {
+  } else {
+    if (imageFlags & NVG_IMAGE_NEAREST) {
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    }
-    else
-    {
+    } else {
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     }
   }
 
-  if (imageFlags & NVG_IMAGE_NEAREST)
-  {
+  if (imageFlags & NVG_IMAGE_NEAREST) {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  }
-  else
-  {
+  } else {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   }
 
@@ -94,8 +83,7 @@ GLNVGtexture::load(int w, int h, int type, const void *data, int imageFlags)
   glPixelStorei(GL_UNPACK_SKIP_ROWS, 0);
 
   // The new way to build mipmaps on GLES and GL3
-  if (imageFlags & NVG_IMAGE_GENERATE_MIPMAPS)
-  {
+  if (imageFlags & NVG_IMAGE_GENERATE_MIPMAPS) {
     glGenerateMipmap(GL_TEXTURE_2D);
   }
 
@@ -105,8 +93,7 @@ GLNVGtexture::load(int w, int h, int type, const void *data, int imageFlags)
 }
 
 std::shared_ptr<GLNVGtexture> GLNVGtexture::fromHandle(GLuint textureId, int w,
-                                                       int h, int imageFlags)
-{
+                                                       int h, int imageFlags) {
   auto tex = std::shared_ptr<GLNVGtexture>(new GLNVGtexture);
   tex->_type = NVG_TEXTURE_RGBA;
   tex->_handle = textureId;
@@ -116,8 +103,7 @@ std::shared_ptr<GLNVGtexture> GLNVGtexture::fromHandle(GLuint textureId, int w,
   return tex;
 }
 
-void GLNVGtexture::update(int x, int y, int w, int h, const void *data)
-{
+void GLNVGtexture::update(int x, int y, int w, int h, const void *data) {
   bind();
 
   glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
@@ -144,17 +130,14 @@ void GLNVGtexture::update(int x, int y, int w, int h, const void *data)
 /// TextureManager
 ///
 unsigned int _boundTexture = {};
-static void glnvg__bindTexture(GLuint tex)
-{
-  if (_boundTexture != tex)
-  {
+static void glnvg__bindTexture(GLuint tex) {
+  if (_boundTexture != tex) {
     _boundTexture = tex;
     glBindTexture(GL_TEXTURE_2D, tex);
   }
 }
 
-TextureManager::TextureManager()
-{
+TextureManager::TextureManager() {
   // Some platforms does not allow to have samples to unset textures.
   // Create empty one which is bound when there's no texture specified.
   {
@@ -166,42 +149,34 @@ TextureManager::TextureManager()
   }
 }
 
-void TextureManager::registerTexture(const std::shared_ptr<GLNVGtexture> &tex)
-{
+void TextureManager::registerTexture(const std::shared_ptr<GLNVGtexture> &tex) {
   _textures.insert(std::make_pair(tex->id(), tex));
 }
 
-std::shared_ptr<GLNVGtexture> TextureManager::findTexture(int id)
-{
+std::shared_ptr<GLNVGtexture> TextureManager::findTexture(int id) {
   auto found = _textures.find(id);
-  if (found != _textures.end())
-  {
+  if (found != _textures.end()) {
     return found->second;
   }
   return {};
 }
 
-bool TextureManager::deleteTexture(int id)
-{
+bool TextureManager::deleteTexture(int id) {
   auto found = _textures.find(id);
-  if (found != _textures.end())
-  {
+  if (found != _textures.end()) {
     _textures.erase(found);
     return true;
   }
   return false;
 }
 
-void TextureManager::bind(int image)
-{
+void TextureManager::bind(int image) {
   std::shared_ptr<GLNVGtexture> tex = NULL;
-  if (image != 0)
-  {
+  if (image != 0) {
     tex = findTexture(image);
   }
   // If no image is set, use empty texture
-  if (tex == NULL)
-  {
+  if (tex == NULL) {
     tex = findTexture(_dummyTex);
   }
   glnvg__bindTexture(tex ? tex->handle() : 0);
